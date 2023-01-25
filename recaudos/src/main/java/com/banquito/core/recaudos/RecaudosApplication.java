df8b8ac1a4c63a7e45be6849042ef716b5ce6753
Abigail Cabascango <51092317+abigailscl@users.noreply.github.com>
2023-01-25 18:43:29 -0500

    Initial commit

---
+++ recaudos/src/main/java/com/banquito/core/recaudos/RecaudosApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.core.recaudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecaudosApplication.class, args);
+	}
+
+}
---
+++ recaudos/src/test/java/com/banquito/core/recaudos/RecaudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.recaudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
